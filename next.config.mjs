8ee73a5a3456279ca0521077f2220d672bb6937c
bourneidBH <bourneid87@gmail.com>
2026-09-22 15:44:09 -0500

    update deploy.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: process.env.PAGES_BASE_PATH,
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
